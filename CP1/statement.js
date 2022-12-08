de1b8bf96b5ac98481ac2e3c99abe956611964e2
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2022-12-08 17:20:36 +0900

    CP1: totalAmount 変数の削除 (ステートメントのスライド)

--- CP1/statement.js
+++ CP1/statement.js
@@ -49,15 +49,14 @@ function statement(invoice, plays) {
     return volumeCredits;
   }
 
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     // 注文の内訳を出力
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
-    totalAmount += amountFor(perf);
   }
+  let totalAmount = 0;
   for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
