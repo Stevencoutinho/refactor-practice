61b3ce07255c6289cda78411c407ba66e53fc6d1
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2022-12-15 08:19:31 +0900

    CP1: ポリモーフィズムによる条件記述の置き換え

--- CP1/createStatementData.js
+++ CP1/createStatementData.js
@@ -53,11 +53,7 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type)
-      result += Math.floor(this.performance.audience / 5);
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 
@@ -78,6 +74,10 @@ class ComedyCalculator extends PerformanceCalculator {
       result += 10000 + 500 * (this.performance.audience - 20);
     }
     return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
 
