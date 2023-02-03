3188572fa78db83abf2179935e8077d662b42ed6
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-02-03 21:29:24 +0900

    Province のテストコードを追加

---
+++ test/provice_test.js
@@ -0,0 +1,10 @@
+const { Province, sampleProvinceData } = require("./../CP4/Province");
+const { describe, it } = require("mocha");
+const assert = require("assert");
+
+describe("province", function () {
+  it("shortfall", function () {
+    const asia = new Province(sampleProvinceData());
+    assert.equal(asia.shortfall, 5);
+  });
+});
