9e581686b3739a988d9f6609c11d075f9dd8d012
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-02-06 13:51:21 +0900

    expect 形式に書き換え

--- test/provice_test.js
+++ test/provice_test.js
@@ -1,14 +1,14 @@
 const { Province, sampleProvinceData } = require("./../CP4/Province");
 const { describe, it } = require("mocha");
-const assert = require("assert");
+const { expect } = require("chai");
 
 describe("province", function () {
   it("shortfall", function () {
     const asia = new Province(sampleProvinceData());
-    assert.equal(asia.shortfall, 5);
+    expect(asia.shortfall).equal(5);
   });
   it("profit", function () {
     const asia = new Province(sampleProvinceData());
-    assert.equal(asia.profit, 230);
+    expect(asia.profit).equal(230);
   });
 });
