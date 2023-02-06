16646db8cfbdcc0a80a120d0619b0ce2e964edf7
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-02-06 13:52:03 +0900

    beforeEach でテスト前に値をクリア

--- test/provice_test.js
+++ test/provice_test.js
@@ -1,14 +1,16 @@
 const { Province, sampleProvinceData } = require("./../CP4/Province");
-const { describe, it } = require("mocha");
+const { describe, it, beforeEach } = require("mocha");
 const { expect } = require("chai");
 
 describe("province", function () {
+  let asia;
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
   it("shortfall", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).equal(5);
   });
   it("profit", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).equal(230);
   });
 });
