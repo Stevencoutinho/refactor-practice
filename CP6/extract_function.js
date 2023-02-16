7ca58999ea90451e9ce3053a3f2937eed780c077
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-02-16 22:24:05 +0900

    例) スコープ外となる変数がない場合

--- CP6/extract_function.js
+++ CP6/extract_function.js
@@ -1,10 +1,24 @@
 function printOwing(invoice) {
-  printBanner();
-  let outstanding = calculateOutstanding();
-  printDetails(outstanding);
+  let outstanding = 0;
 
-  function printDetails(outstanding) {
-    console.log(`name: ${invoice.customer}`);
-    console.log(`amount: ${outstanding}`);
+  console.log("***********************");
+  console.log("**** Customer Owes ****");
+  console.log("***********************");
+
+  // 未払い金の計算 (calculate outstanding)
+  for (const o of invoice.orders) {
+    outstanding += o.amount;
   }
+
+  // 締め日の記録 (record due date)
+  const today = Clock.today;
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth().today.getDate() + 30
+  );
+
+  // 明細の印字 (print details)
+  console.log(`name: ${invoice.customer}`);
+  console.log(`amount: ${outstanding}`);
+  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
 }
