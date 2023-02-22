23b1c32cf706d798cf6b8cf82106f63319978e8e
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-02-22 21:45:18 +0900

    テストコードの追加

--- test/extract_variable_test.js
+++ test/extract_variable_test.js
@@ -1,9 +1,19 @@
-const { price } = require("./../CP6/extract_variable");
-const { describe, it } = require("mocha");
+const { price, Order } = require("./../CP6/extract_variable");
+const { describe, it, beforeEach } = require("mocha");
 const { expect } = require("chai");
 
 describe("price 関数のテスト", function () {
   it("数量: 10, 単価: ¥500", function () {
     expect(price({ quantity: 10, itemPrice: 500 })).equal(5100);
+  });
+});
+
+describe("Order クラスのテスト", function () {
+  let order;
+  beforeEach(function () {
+    order = new Order({ quantity: 10, itemPrice: 500 });
+  });
+  it("price", function () {
+    expect(order.price).equal(5100);
   });
 });
