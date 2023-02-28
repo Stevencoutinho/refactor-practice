57c036705a497de0f973114e93f671a935d50bae
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-02-28 20:57:45 +0900

    古い関数を削除

--- CP6/change_function_declaration.js
+++ CP6/change_function_declaration.js
@@ -1,12 +1,7 @@
-function circum(radius) {
-  return circumference(radius);
-}
-
 function circumference(radius) {
   return 2 * Math.PI * radius;
 }
 
 module.exports = {
-  circum,
   circumference,
 };
