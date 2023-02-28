466eded429fc8baa50d03cc9848e4e95f3bdc2a1
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-02-28 20:57:56 +0900

    テストコードを削除

--- test/change_function_declaration.test.js
+++ test/change_function_declaration.test.js
@@ -1,12 +1,6 @@
-const { circum, circumference } = require("../CP6/change_function_declaration");
+const { circumference } = require("../CP6/change_function_declaration");
 const { describe, it } = require("mocha");
 const { expect } = require("chai");
-
-describe("circum 関数のテスト", function () {
-  it("半径 10", function () {
-    expect(circum(10)).equal(62.83185307179586);
-  });
-});
 
 describe("circumference 関数のテスト", function () {
   it("半径 10", function () {
