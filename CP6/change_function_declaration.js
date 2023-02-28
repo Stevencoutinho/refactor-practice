10106e5128eacf334279601541cbd09e680a8b53
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-02-28 21:07:31 +0900

    関数のインライン化

--- CP6/change_function_declaration.js
+++ CP6/change_function_declaration.js
@@ -4,7 +4,7 @@ function circumference(radius) {
 
 class Book {
   addReservation(customer) {
-    this._reservations.push(customer);
+    this.zz_addReservation(customer);
   }
 
   zz_addReservation(customer) {
