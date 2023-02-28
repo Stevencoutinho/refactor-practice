52583a108e5d805713bce98fb8587ea75382560b
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-02-28 21:02:41 +0900

    例: パラメータの追加

--- CP6/change_function_declaration.js
+++ CP6/change_function_declaration.js
@@ -2,6 +2,12 @@ function circumference(radius) {
   return 2 * Math.PI * radius;
 }
 
+class Book {
+  addReservation(customer) {
+    this._reservations.push(customer);
+  }
+}
+
 module.exports = {
   circumference,
 };
