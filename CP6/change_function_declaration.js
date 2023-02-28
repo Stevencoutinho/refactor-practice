b2436af3324c60c0dc7265d82793bc9244a03210
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-02-28 21:24:32 +0900

    例: パラメータをプロパティに変更する

--- CP6/change_function_declaration.js
+++ CP6/change_function_declaration.js
@@ -4,6 +4,10 @@ function circumference(radius) {
   return 2 * Math.PI * radius;
 }
 
+function inNewEngland(aCustomer) {
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(aCustomer.address.state);
+}
+
 class Book {
   addReservation(customer) {
     this.zz_addReservation(customer, false);
