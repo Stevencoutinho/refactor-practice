8c82d9fcddc9fec1661c4f12167fa2146514427c
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-02-28 21:47:06 +0900

    データを読み書きする関数を定義

--- CP6/encapsulate_variable.js
+++ CP6/encapsulate_variable.js
@@ -3,3 +3,11 @@ let defaultOwner = { firstName: "Martin", lastName: "Fowler" };
 spaceship.owner = defaultOwner;
 
 defaultOwner = { firstName: "Rebecca", lastName: "Parsons" };
+
+function getDefaultOwner() {
+  return defaultOwner;
+}
+
+function setDefaultOwner(arg) {
+  defaultOwner = arg;
+}
