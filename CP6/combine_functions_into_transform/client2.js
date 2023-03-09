7d9a420a5cfaf6490be168203cc11583a3abb054
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-03-09 21:41:04 +0900

    フィールドに置き換え

--- CP6/combine_functions_into_transform/client2.js
+++ CP6/combine_functions_into_transform/client2.js
@@ -3,7 +3,4 @@ const { enrichReading } = require("./combine_functions_into_transform");
 // client 2
 const rawReading = acquireReading();
 const aReading = enrichReading(rawReading);
-const taxableCharge = Math.max(
-  0,
-  aReading.baseCharge - taxThreshold(aReading.year)
-);
+const taxableCharge = aReading.taxableCharge;
