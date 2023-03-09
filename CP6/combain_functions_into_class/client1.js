71d03e9b828ee4aad9ba869ba1303322092cf8fb
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-03-09 20:42:53 +0900

    メソッドを呼び出し

--- CP6/combain_functions_into_class/client1.js
+++ CP6/combain_functions_into_class/client1.js
@@ -1,3 +1,6 @@
+const { Reading } = require("./combine_functions_into_class");
+
 // client 1
-const aReading = acquireReading();
-const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+const baseCharge = aReading.baseCharge;
