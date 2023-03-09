d67200009dbe4aa8141b2aec6a8c20f9af79b6a2
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-03-09 20:40:34 +0900

    関数名の変更

--- CP6/combain_functions_into_class/client3.js
+++ CP6/combain_functions_into_class/client3.js
@@ -3,4 +3,4 @@ const { Reading } = require("./combine_functions_into_class");
 // client 3
 const rawReading = acquireReading();
 const aReading = new Reading(rawReading);
-const basicChargeAmount = aReading.calculateBaseCharge;
+const basicChargeAmount = aReading.baseCharge;
