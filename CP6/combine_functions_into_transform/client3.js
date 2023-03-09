d65419e98ffcb0547aba4952570c0bca092888e4
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-03-09 21:29:37 +0900

    フィールドに置き換え

--- CP6/combine_functions_into_transform/client3.js
+++ CP6/combine_functions_into_transform/client3.js
@@ -3,8 +3,4 @@ const { enrichReading } = require("./combine_functions_into_transform");
 // client 3
 const rawReading = acquireReading();
 const aReading = enrichReading(rawReading);
-const basicChargeAmount = calculateBaseCharge(aReading);
-
-function calculateBaseCharge(aReading) {
-  return baseRate(aReading.month, aReading.year) * aReading.quantity;
-}
+const basicChargeAmount = aReading.baseCharge;
