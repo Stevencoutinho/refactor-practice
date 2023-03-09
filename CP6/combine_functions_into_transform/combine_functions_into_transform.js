0178bf41ec43abf414dbde27e373afab8cb06343
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-03-09 21:21:02 +0900

    関数の移動

--- CP6/combine_functions_into_transform/combine_functions_into_transform.js
+++ CP6/combine_functions_into_transform/combine_functions_into_transform.js
@@ -1,4 +1,9 @@
+function calculateBaseCharge(aReading) {
+  return baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
+
 export function enrichReading(original) {
   const result = _.cloneDeep(original);
+  result.baseCharge = calculateBaseCharge(result);
   return result;
 }
