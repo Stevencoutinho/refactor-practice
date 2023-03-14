bd9b098c4b78c96965d7e8299ae59edb65d5145d
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-03-14 14:37:03 +0900

    関数の抽出

--- CP6/split_phase.js
+++ CP6/split_phase.js
@@ -1,16 +1,20 @@
 function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+  const price = applyShipping(priceData, shippingMethod);
+  return price;
+}
+
+function calculatePricingData(product, quantity) {
   const basePrice = product.basePrice * quantity;
   const discount =
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
-  const priceData = {
+  return {
     basePrice: basePrice,
     quantity: quantity,
     discount: discount,
   };
-  const price = applyShipping(priceData, shippingMethod);
-  return price;
 }
 
 function applyShipping(priceData, shippingMethod) {
