343e93d80ea814f8530c04946700b750e0477464
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-03-14 14:16:31 +0900

    例: フェーズの分離

---
+++ CP6/split_phase.js
@@ -0,0 +1,14 @@
+function priceOrder(product, quantity, shippingMethod) {
+  const basePrise = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  const shippingPerCase =
+    basePrise > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFee
+      : shippingMethod.feePerCase;
+  const shippingCost = quantity * shippingPerCase;
+  const price = basePrice - discount + shippingCost;
+  return price;
+}
