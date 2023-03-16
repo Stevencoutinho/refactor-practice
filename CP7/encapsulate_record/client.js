45ce8caf7c5a16b03a5bc68c236d46c2a4d56a3d
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-03-16 21:08:10 +0900

    参照側を更新

--- CP7/encapsulate_record/client.js
+++ CP7/encapsulate_record/client.js
@@ -1,6 +1,6 @@
-const { organization } = require("./encapsulate_record.js");
+const { getRawDataOfOrganization } = require("./encapsulate_record.js");
 
-const result = `<h1>${organization.name}</h1>`;
+const result = `<h1>${getRawDataOfOrganization().name}</h1>`;
 
 const newName = "Steven coutinho";
-organization.name = newName;
+getRawDataOfOrganization().name = newName;
