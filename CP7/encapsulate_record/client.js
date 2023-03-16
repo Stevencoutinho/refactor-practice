f0c453c3f66350694e744c1e84f541da1eeddc23
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-03-16 21:23:31 +0900

    ファイル名を変更

--- CP7/encapsulate_record/client.js
+++ CP7/encapsulate_record/client.js
@@ -1,4 +1,4 @@
-const { getOrganization } = require("./encapsulate_record.js");
+const { getOrganization } = require("./encapsulate_record1.js");
 
 const result = `<h1>${getOrganization().name}</h1>`;
 
