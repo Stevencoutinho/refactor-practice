f32bcfd8d22d99d7054c7c39c6fa085b4e80ec1f
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-03-16 21:40:56 +0900

    参照側を変更

--- CP7/encapsulate_record/client2.js
+++ CP7/encapsulate_record/client2.js
@@ -1,4 +1,8 @@
-const { customerData } = require("./encapsulate_record2");
+const {
+  customerData,
+  getRawDataOfCustomers,
+  setRawDataOfCustomers,
+} = require("./encapsulate_record2");
 
 const customerID = "1920";
 const year = 2015;
@@ -6,11 +10,12 @@ const month = 1;
 const amount = 100;
 
 // sample update...
-customerData[customerID].usages[year][month] = amount;
+getRawDataOfCustomers()[customerID].usages[year][month] = amount;
 
 // sample read...
 function compareUsage(customerID, laterYear, month) {
-  const later = customerData[customerID].usages[laterYear][month];
-  const earlier = customerData[customerID].usages[laterYear - 1][month];
+  const later = getRawDataOfCustomers()[customerID].usages[laterYear][month];
+  const earlier =
+    getRawDataOfCustomers()[customerID].usages[laterYear - 1][month];
   return { laterAmount: later, change: later - earlier }; // 前年同月比較
 }
