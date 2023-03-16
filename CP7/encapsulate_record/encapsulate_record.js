e543bb79ece4cac5fedc661d92a1cb4c13aad70b
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-03-16 21:10:29 +0900

    クラスを作成

--- CP7/encapsulate_record/encapsulate_record.js
+++ CP7/encapsulate_record/encapsulate_record.js
@@ -3,3 +3,9 @@ export const organization = { name: "Acme Gooseberries", country: "GB" };
 export function getRawDataOfOrganization() {
   return organization;
 }
+
+class Organization {
+  constructor(data) {
+    this._data = data;
+  }
+}
