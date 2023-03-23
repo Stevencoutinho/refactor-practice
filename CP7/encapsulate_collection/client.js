2a2c029bfed7669debb0936ed2408b49505af87b
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-03-23 20:19:28 +0900

    クライアントコードを編集

--- CP7/encapsulate_collection/client.js
+++ CP7/encapsulate_collection/client.js
@@ -1,4 +1,7 @@
-const { Person } = require("./encapsulate_collection");
+const { Person, Course } = require("./encapsulate_collection");
 
 const aPerson = new Person("Steven");
 const numAdvancedCourses = aPerson.courses.filter((c) => c.isAdvanced).length;
+
+const basicCourseNames = readBasicCourseNames("hoge");
+aPerson.courses = basicCourseNames.map((name) => new Course(name, false));
