7a2bc76ad9a02098224179c35f65b45067c0ee6f
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-03-28 18:10:12 +0900

    フィールドの移動

--- CP7/extract_class/extract_class.js
+++ CP7/extract_class/extract_class.js
@@ -26,4 +26,11 @@ class Person {
   }
 }
 
-class TelephoneNumber {}
+class TelephoneNumber {
+  get officeAreaCode() {
+    return this._officeAreaCode;
+  }
+  set officeAreaCode(arg) {
+    this._officeAreaCode = arg;
+  }
+}
