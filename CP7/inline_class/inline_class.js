250630afcca3eb7d7c0bde5c92c771e70f767ff5
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-03-29 19:44:21 +0900

    クラスを削除

--- CP7/inline_class/inline_class.js
+++ CP7/inline_class/inline_class.js
@@ -1,26 +1,3 @@
-class TrackingInformation {
-  constructor(data) {
-    this._shippingCompany = data.shippingCompany;
-    this._trackingNumber = data.trackingNumber;
-  }
-
-  get shippingCompany() {
-    return this._shippingCompany;
-  }
-  set shippingCompany(arg) {
-    this._shippingCompany = arg;
-  }
-  get trackingNumber() {
-    return this._trackingNumber;
-  }
-  set trackingNumber(arg) {
-    this._trackingNumber = arg;
-  }
-  get display() {
-    return `${this.shippingCompany}: ${this.trackingNumber}`;
-  }
-}
-
 export class Shipment {
   constructor(data) {
     this._shippingCompany = data.shippingCompany;
