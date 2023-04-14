3026ccc80c6944619df77d046e831382c2147aa6
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-04-14 17:56:50 +0900

    ステートメントのスライド

--- CP8/move_field/move_field.js
+++ CP8/move_field/move_field.js
@@ -1,8 +1,8 @@
 class Customer {
   constructor(name, discountRate) {
     this._name = name;
-    this._setDiscountRate(discountRate);
     this._contract = new CustomerContract(dateToday());
+    this._setDiscountRate(discountRate);
   }
 
   get discountRate() {
