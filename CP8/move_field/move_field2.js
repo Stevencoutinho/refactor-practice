b9c1ef64edcb619a9ff3a10285c5384cf05aaf0c
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-04-14 18:09:34 +0900

    アクセサメソッドを置き換える

--- CP8/move_field/move_field2.js
+++ CP8/move_field/move_field2.js
@@ -1,12 +1,11 @@
 class Account {
-  constructor(number, type, interestRate) {
+  constructor(number, type) {
     this._number = number;
     this._type = type;
-    this._interestRate = interestRate;
     assert(interestRate === this._type.interestRate);
   }
   get interestRate() {
-    return this._interestRate;
+    return this._type.interestRate;
   }
 }
 
