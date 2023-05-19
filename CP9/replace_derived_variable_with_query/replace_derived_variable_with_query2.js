b3c5cbd08cb92174b87df3c8adc287ccb7afe056
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-05-19 13:59:31 +0900

    アサーションの導入

--- CP9/replace_derived_variable_with_query/replace_derived_variable_with_query2.js
+++ CP9/replace_derived_variable_with_query/replace_derived_variable_with_query2.js
@@ -5,10 +5,17 @@ class ProductionPlan {
     this._adjustments = [];
   }
   get production() {
+    assert(
+      this._productionAccumulator === this.calculatedProductionAccumulator
+    );
     return this._initialProduction + this._productionAccumulator;
   }
   applyAdjustment(anAdjustment) {
     this._adjustments.push(anAdjustment);
     this._production += anAdjustment.amount;
+  }
+
+  get calculatedProductionAccumulator() {
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
   }
 }
