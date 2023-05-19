a91afd24d803be99fdca1f6cac89731147354481
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-05-19 13:48:43 +0900

    関数のインライン化

--- CP9/replace_derived_variable_with_query/replace_derived_variable_with_query.js
+++ CP9/replace_derived_variable_with_query/replace_derived_variable_with_query.js
@@ -1,14 +1,9 @@
 class ProductionPlan {
   get production() {
-    assert(this._production === this.calculatedProduction);
-    return this._production;
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
   }
   applyAdjustment(anAdjustment) {
     this._adjustments.push(anAdjustment);
     this._production += anAdjustment.amount;
-  }
-
-  get calculatedProduction() {
-    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
   }
 }
