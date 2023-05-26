64dfb46378e22fd9fd8a526719da921ab492290c
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-05-26 12:02:12 +0900

    then 節を関数に抽出する

--- CP10/decompose_conditional/decompose_conditional.js
+++ CP10/decompose_conditional/decompose_conditional.js
@@ -1,9 +1,12 @@
 if (summer()) {
-  charge = quantity * plan.sumerRate;
+  charge = summerCharge();
 } else {
   charge = quantity * plan.regularRate + plan.regularServiceCharge;
 }
 
 function summer() {
   return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+}
+function summerCharge() {
+  return quantity * plan.summerRate;
 }
