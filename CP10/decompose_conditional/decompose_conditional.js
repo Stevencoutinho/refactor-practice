077312483714da0ecfeb55373505be6753e2ba0f
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-05-26 12:00:40 +0900

    条件記述を関数に抽出する

--- CP10/decompose_conditional/decompose_conditional.js
+++ CP10/decompose_conditional/decompose_conditional.js
@@ -1,5 +1,9 @@
-if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd)) {
+if (summer()) {
   charge = quantity * plan.sumerRate;
 } else {
   charge = quantity * plan.regularRate + plan.regularServiceCharge;
+}
+
+function summer() {
+  return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
 }
