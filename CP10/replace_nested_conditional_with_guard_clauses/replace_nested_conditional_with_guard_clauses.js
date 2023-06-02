1c0731fd22703c90b67325ccdd4b34b983e080a3
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-06-02 15:01:57 +0900

    ガード節に置き換える

--- CP10/replace_nested_conditional_with_guard_clauses/replace_nested_conditional_with_guard_clauses.js
+++ CP10/replace_nested_conditional_with_guard_clauses/replace_nested_conditional_with_guard_clauses.js
@@ -1,17 +1,12 @@
 function payAmount(employee) {
   let result;
   if (employee.isSeparated) return { amount: 0, reasonCode: "SEP" };
-  else {
-    if (employee.isRetired) {
-      result = { amount: 0, reasonCode: "RET" };
-    } else {
-      // 金額を計算するロジック
-      lorem.ipsum(dolor.sitAmet);
-      consectetur(adipiscing).elit();
-      sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(manga.aliqua);
-      ut.enim.ad(minim.veniam);
-      result = someFinalComputation();
-    }
-  }
+  if (employee.isRetired) return { amount: 0, reasonCode: "RET" };
+  // 金額を計算するロジック
+  lorem.ipsum(dolor.sitAmet);
+  consectetur(adipiscing).elit();
+  sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(manga.aliqua);
+  ut.enim.ad(minim.veniam);
+  result = someFinalComputation();
   return result;
 }
