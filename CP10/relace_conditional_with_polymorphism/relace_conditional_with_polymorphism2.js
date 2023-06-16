660a8e7d5d378e2a88d48bd281b8b13f3e2fed80
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-06-16 17:20:59 +0900

    関数の抽出

--- CP10/relace_conditional_with_polymorphism/relace_conditional_with_polymorphism2.js
+++ CP10/relace_conditional_with_polymorphism/relace_conditional_with_polymorphism2.js
@@ -61,6 +61,19 @@ class Rating {
     }
     return result;
   }
+  get voyageAndHistoryLengthFactor() {
+    let result = 0;
+    if (this.voyage.zone === "china" && this.hasChinaHistory) {
+      result += 3;
+      if (this.history.length > 10) result += 1;
+      if (this.voyage.length > 12) result += 1;
+      if (this.voyage.length > 18) result -= 1;
+    } else {
+      if (this.history.length > 8) result += 1;
+      if (this.voyage.length > 14) result -= 1;
+    }
+    return result;
+  }
   get hasChinaHistory() {
     return this.history.some((v) => "china" === v.zone);
   }
