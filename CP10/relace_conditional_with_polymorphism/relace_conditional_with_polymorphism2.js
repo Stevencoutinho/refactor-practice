63618299b2794fbe460320a06aeccd8444a45058
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-06-16 17:52:53 +0900

    デッドコードの削除

--- CP10/relace_conditional_with_polymorphism/relace_conditional_with_polymorphism2.js
+++ CP10/relace_conditional_with_polymorphism/relace_conditional_with_polymorphism2.js
@@ -75,7 +75,6 @@ class ExperiencedChinaRating extends Rating {
   }
   get voyageLengthFactor() {
     let result = 0;
-    result += 3;
     if (this.voyage.length > 12) result += 1;
     if (this.voyage.length > 18) result -= 1;
     return result;
