d1cb4a9575193cec83d24b6d4149d1dd5a423d76
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-06-20 15:07:59 +0900

    クライアントコードの追加

--- CP10/introduce_special_case/introduce_special_case.js
+++ CP10/introduce_special_case/introduce_special_case.js
@@ -1,3 +1,25 @@
+// client 1
+const aCustomer = site.customer;
+// 省略...
+let customerName;
+if (aCustomer === "unknown") customerName = "occupant";
+else customerName = aCustomer.name;
+
+// client 2
+const plan =
+  aCustomer === "unknown"
+    ? CustomElementRegistry.billingPlans.basic
+    : aCustomer.billingPlan;
+
+// client 3
+if (aCustomer !== "unknown") aCustomer.billingPlan = newPlan;
+
+// client 4
+const weeksDelinquent =
+  aCustomer === "unknown"
+    ? 0
+    : aCustomer.paymentHistory.weeksDelinquentInLastYear;
+
 class Site {
   get customer() {
     return this._customer;
