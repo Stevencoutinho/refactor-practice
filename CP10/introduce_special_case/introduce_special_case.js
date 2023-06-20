8ed5322537d95410e13eedf666978f17c1913ecc
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-06-20 15:03:25 +0900

    特殊ケースの導入 例1

---
+++ CP10/introduce_special_case/introduce_special_case.js
@@ -0,0 +1,20 @@
+class Site {
+  get customer() {
+    return this._customer;
+  }
+}
+
+class Customer {
+  get name() {
+    return this._name;
+  }
+  get billingPlan() {
+    return this._billingPlan;
+  }
+  set billingPlan(arg) {
+    this._nillingPlan = arg;
+  }
+  get paymentHistory() {
+    return this._paymentHistory;
+  }
+}
