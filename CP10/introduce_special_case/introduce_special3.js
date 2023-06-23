589ccb2ad2fe520e7ed7e4c0d0b0e64614a62810
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-06-23 12:43:26 +0900

    例3: 変換を利用する

---
+++ CP10/introduce_special_case/introduce_special3.js
@@ -0,0 +1,21 @@
+const record = {
+  name: "Acme Boston",
+  location: "Malen MA",
+  // 場所についての詳細が続く
+  customer: {
+    name: "Acme Industries",
+    billingPlan: "plan-451",
+    paymentHistory: {
+      weeksDelinquentInLastYear: 7,
+      // さらに続く
+    },
+    // さらに続く
+  },
+};
+
+const record2 = {
+  name: "Warehouse Unit 15",
+  location: "Malden MA",
+  // 場所についての詳細が続く
+  customer: "unknown",
+};
