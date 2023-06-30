04edb75e3b8b8fefa76a4c48a0641aac8efc0b08
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-06-30 15:27:34 +0900

    関数の追加

--- CP11/remove_flag_argument/remove_flag_argument.js
+++ CP11/remove_flag_argument/remove_flag_argument.js
@@ -3,6 +3,23 @@ function deliveryDate(anOrder, isRush) {
   else return regularDeliveryDate(anOrder);
 }
 
+function deliveryDate2(anOrder, isRush) {
+  let result;
+  let deliveryTime;
+  if (anOrder.deliveryState === "MA" || anOrder.deliveryState === "CT")
+    deliveryTime = isRush ? 1 : 2;
+  else if (anOrder.deliveryState === "NY" || anOrder.deliveryState === "NH") {
+    deliveryTime = 2;
+    if (anOrder.deliveryState === "NH" && !isRush) deliveryTime = 3;
+  } else if (isRush) {
+    deliveryTime = 3;
+  } else if (anOrder.deliveryState === "ME") deliveryTime = 3;
+  else deliveryTime = 4;
+  result = anOrder.placedOn.plusDays(2 + deliveryTime);
+  if (isRush) result = result.minusDays(1);
+  return result;
+}
+
 function rushDeliveryDate(anOrder) {
   let deliveryTime;
   if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
