4a860b03d5a5e99c61ffabf417cf1a9de013cfac
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-07-03 17:54:35 +0900

    関数の移動

--- CP11/preserve_whole_object/preserve_whole_object2.js
+++ CP11/preserve_whole_object/preserve_whole_object2.js
@@ -1,16 +1,15 @@
 class HeatingPlan {
+  xxNEWwithinRange(aPlan, tempRange) {
+    const low = tempRange.low;
+    const high = tempRange.high;
+    const isWithinRange = aPlan.withinRange(low, high);
+    return isWithinRange;
+  }
   withinRange(low, high) {
     return low >= this.temperatureRange.low && high <= temperatureRange.high;
   }
 }
 
-function xxNEWwithinRange(aPlan, tempRange) {
-  const low = tempRange.low;
-  const high = tempRange.high;
-  const isWithinRange = aPlan.withinRange(low, high);
-  return isWithinRange;
-}
-
 const tempRange = aRoom.daysTempRange;
-const isWithinRange = xxNEWwithinRange(aPlan, tempRange);
+const isWithinRange = aPlan.xxNEWwithinRange(aPlan, tempRange);
 if (!isWithinRange) alerts.push("室温が設定値を超えました");
