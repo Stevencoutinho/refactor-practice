8c070da58a8c64f6c34e247097b9d8d1138915c6
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-07-03 17:34:00 +0900

    既存の関数を呼び出す

--- CP11/preserve_whole_object/preserve_whole_object.js
+++ CP11/preserve_whole_object/preserve_whole_object.js
@@ -1,5 +1,7 @@
 class HeatingPlan {
-  xxNEWwithinRange(aNumberRange) {}
+  xxNEWwithinRange(aNumberRange) {
+    return this.withinRange(aNumberRange.low, aNumberRange.high);
+  }
   withinRange(bottom, top) {
     return (
       bottom >= this.temperatureRange.low && top < +this.temperatureRange.high
