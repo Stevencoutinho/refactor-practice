92fa8e13ae430b9d7776b9ee2f4990361066fb35
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-07-03 17:35:36 +0900

    デッドコードの削除

--- CP11/preserve_whole_object/preserve_whole_object.js
+++ CP11/preserve_whole_object/preserve_whole_object.js
@@ -9,7 +9,5 @@ class HeatingPlan {
   }
 }
 
-const low = aRoom.daysTempRange.low;
-const high = aRoom.daysTempRange.high;
 if (!aPlan.xxNEWwithinRange(aRoom.daysTempRange))
   alerts.push("室温が設定値を超えました");
