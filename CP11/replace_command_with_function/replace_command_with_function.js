ba0a6b73603ef77af1f5c8a3614613ce4e168889
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-07-28 14:21:18 +0900

    デッドコードの削除

--- CP11/replace_command_with_function/replace_command_with_function.js
+++ CP11/replace_command_with_function/replace_command_with_function.js
@@ -1,15 +1,3 @@
-class ChargeCalculator {
-  constructor(customer, usage, provider) {}
-
-  get baseCharge() {
-    return this._customer.baseCharge * this._usage;
-  }
-  charge(customer, usage, provider) {
-    const baseCharge = customer.baseRate * usage;
-    return baseCharge + provider.connectionCharge;
-  }
-}
-
 function charge(customer, usage, provider) {
   const baseCharge = customer.baseRate * usage;
   return baseCharge + provider.connectionCharge;
