464d1d4c51d7b338126f3b6f48ae9743b5bf4fc9
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-07-28 13:58:39 +0900

    関数によるコマンドの置き換え

---
+++ CP11/replace_command_with_function/replace_command_with_function.js
@@ -0,0 +1,14 @@
+class ChargeCalculator {
+  constructor(customer, usage, provider) {
+    this._customer = customer;
+    this._usage = usage;
+    this._provider = provider;
+  }
+
+  get baseCharge() {
+    return this._customer.baseCharge * this._usage;
+  }
+  get charge() {
+    return this._baseCharge + this._provider.connectionCharge;
+  }
+}
