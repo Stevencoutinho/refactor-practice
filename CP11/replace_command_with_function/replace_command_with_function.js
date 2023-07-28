4ff9c382d9128b65e2fd1e526cab2ffc4c738ac3
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-07-28 14:19:18 +0900

    関数パラメータに置き換える

--- CP11/replace_command_with_function/replace_command_with_function.js
+++ CP11/replace_command_with_function/replace_command_with_function.js
@@ -1,6 +1,5 @@
 class ChargeCalculator {
   constructor(customer, usage, provider) {
-    this._usage = usage;
     this._provider = provider;
   }
 
@@ -8,7 +7,7 @@ class ChargeCalculator {
     return this._customer.baseCharge * this._usage;
   }
   charge(customer, usage, provider) {
-    const baseCharge = customer.baseRate * this._usage;
+    const baseCharge = customer.baseRate * usage;
     return baseCharge + this._provider.connectionCharge;
   }
 }
