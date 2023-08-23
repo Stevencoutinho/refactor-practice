16a149f5388e9e8fd99654fa9bdaddea767aaa1e
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-08-23 19:48:07 +0900

    関数宣言の変更

--- CP12/replace_type_code_with_subclasses/replace_type_code_with_subclasses.js
+++ CP12/replace_type_code_with_subclasses/replace_type_code_with_subclasses.js
@@ -1,5 +1,5 @@
 class Employee {
-  constructor(name, type) {
+  constructor(name) {
     this._name = name;
   }
   validateType(arg) {
@@ -33,11 +33,11 @@ class Manager extends Employee {
 function createEmployee(name, type) {
   switch (type) {
     case "engineer":
-      return new Engineer(name, type);
+      return new Engineer(name);
     case "salesman":
-      return new Salesman(name, type);
+      return new Salesman(name);
     case "manager":
-      return new Manager(name, type);
+      return new Manager(name);
     default:
       throw new Error(`Employee cannot be of type ${type}`);
   }
