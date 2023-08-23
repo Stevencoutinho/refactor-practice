dae647ca0280798477aeadb48d4ae3ff34b7e78a
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-08-23 19:47:25 +0900

    ファクトリ関数に例外処理を追加

--- CP12/replace_type_code_with_subclasses/replace_type_code_with_subclasses.js
+++ CP12/replace_type_code_with_subclasses/replace_type_code_with_subclasses.js
@@ -1,6 +1,5 @@
 class Employee {
   constructor(name, type) {
-    this.validateType(type);
     this._name = name;
   }
   validateType(arg) {
@@ -39,6 +38,7 @@ function createEmployee(name, type) {
       return new Salesman(name, type);
     case "manager":
       return new Manager(name, type);
+    default:
+      throw new Error(`Employee cannot be of type ${type}`);
   }
-  return new Employee(name, type);
 }
