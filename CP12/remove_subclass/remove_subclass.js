2874dbeac0e3a72d39f9d640127ddb53d8cb3803
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-08-24 18:21:52 +0900

    クライアントコードの追加

--- CP12/remove_subclass/remove_subclass.js
+++ CP12/remove_subclass/remove_subclass.js
@@ -35,5 +35,24 @@ function createFemale(name) {
   return new Female(name);
 }
 
+function loadFromInput(data) {
+  const result = [];
+  data.forEach((aRecord) => {
+    let p;
+    switch (aRecord.gender) {
+      case "M":
+        p = new Male(aRecord.name);
+        break;
+      case "F":
+        p = new Female(aRecord.name);
+        break;
+      default:
+        p = new Person(aRecord.name);
+    }
+    result.push(p);
+  });
+  return result;
+}
+
 // client
 const numberOfMales = people.filter((p) => p instanceof Male).length;
