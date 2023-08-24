ad5740fb463bffb6f16b74bf5dd39b2591d430be
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-08-24 18:23:46 +0900

    関数の抽出

--- CP12/remove_subclass/remove_subclass.js
+++ CP12/remove_subclass/remove_subclass.js
@@ -35,21 +35,25 @@ function createFemale(name) {
   return new Female(name);
 }
 
+function createPerson(aRecord) {
+  let p;
+  switch (aRecord.gender) {
+    case "M":
+      p = new Male(aRecord.name);
+      break;
+    case "F":
+      p = new Female(aRecord.name);
+      break;
+    default:
+      p = new Person(aRecord.name);
+  }
+  return p;
+}
+
 function loadFromInput(data) {
   const result = [];
   data.forEach((aRecord) => {
-    let p;
-    switch (aRecord.gender) {
-      case "M":
-        p = new Male(aRecord.name);
-        break;
-      case "F":
-        p = new Female(aRecord.name);
-        break;
-      default:
-        p = new Person(aRecord.name);
-    }
-    result.push(p);
+    result.push(createPerson(aRecord));
   });
   return result;
 }
