563ea01ce0ef0934c884ebd22e0278b7f46f87d7
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-09-07 21:47:26 +0900

    デッドコードの削除

--- CP12/replace_subbclass_with_delegate/replace_subbclass_with_delegate2.js
+++ CP12/replace_subbclass_with_delegate/replace_subbclass_with_delegate2.js
@@ -59,20 +59,6 @@ class AfricanSwallowDelegate extends SpeciesDelegate {
   }
 }
 
-class NorwegianBlueParrot extends Bird {
-  constructor(data) {
-    super(data);
-    this._voltage = data.voltage;
-    this._isNailed = data.isNailed;
-  }
-  get plumage() {
-    return this._speciesDelegate.plumage;
-  }
-  get airSpeedVelocity() {
-    return this._speciesDelegate.airSpeedVelocity;
-  }
-}
-
 class NorwegianBlueParrotDelegate extends SpeciesDelegate {
   constructor(data, bird) {
     super(data, bird);
