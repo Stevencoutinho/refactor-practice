6b26f058fc09b3eb843d8413251ae10cc2ebf1ae
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-09-07 21:18:35 +0900

    メソッドを追加

--- CP12/replace_subbclass_with_delegate/replace_subbclass_with_delegate2.js
+++ CP12/replace_subbclass_with_delegate/replace_subbclass_with_delegate2.js
@@ -15,6 +15,7 @@ class Bird {
   constructor(data) {
     this._name = data.name;
     this._plumage = data.plumage;
+    this._speciesDelegate = this.selectSpeciesDelegate(data);
   }
   get name() {
     return this._name;
@@ -24,6 +25,14 @@ class Bird {
   }
   get airSpeedVelocity() {
     return null;
+  }
+  selectSpeciesDelegate(data) {
+    switch (data.type) {
+      case "EuropeanSwallow":
+        return new EuropeanSwallowDelegate();
+      default:
+        return null;
+    }
   }
 }
 
