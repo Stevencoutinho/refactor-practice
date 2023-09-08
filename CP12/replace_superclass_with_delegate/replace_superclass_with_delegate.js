05653cbdd237596bebd50c93a40935e36386fd07
ishibashi kazuya <k.ishibashi@sustineri.co.jp>
2023-09-08 13:49:34 +0900

    メソッドの追加

--- CP12/replace_superclass_with_delegate/replace_superclass_with_delegate.js
+++ CP12/replace_superclass_with_delegate/replace_superclass_with_delegate.js
@@ -21,6 +21,15 @@ class Scroll extends CatalogItem {
     this._catalogItem = new CatalogItem(id, title, tags);
     this._lastCleaned = dataLastCleaned;
   }
+  get id() {
+    return this._catalogItem.id;
+  }
+  get title() {
+    return this._catalogItem.title;
+  }
+  hasTag(aString) {
+    return this._catalogItem.hasTag(aString);
+  }
   needsCleaning(targetDate) {
     const threshold = this.hasTag("revered") ? 700 : 1500;
     return this.daysSinceLastCleaning(targetDate) > threshold;
